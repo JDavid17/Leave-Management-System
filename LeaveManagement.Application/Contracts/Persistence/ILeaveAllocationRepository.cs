552d8622f9dd1d94c19ed0eb788e2b54690740e9
Joel Cruz <joel.cruz@bbconsult.co.uk>
2023-11-30 23:39:22 +0100

    Added LeaveManagement.Application Project and GenericRepository

---
+++ LeaveManagement.Application/Contracts/Persistence/ILeaveAllocationRepository.cs
@@ -0,0 +1,7 @@
+﻿using LeaveManagement.Domain;
+
+namespace LeaveManagement.Application.Contracts.Persistence;
+
+public interface ILeaveAllocationRepository : IGenericRepository<LeaveAllocation>
+{
+}
---
+++ LeaveManagement.Application/Contracts/Persistence/ILeaveTypeRepository.cs
@@ -0,0 +1,7 @@
+﻿using LeaveManagement.Domain;
+
+namespace LeaveManagement.Application.Contracts.Persistence;
+
+public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+{
+}
