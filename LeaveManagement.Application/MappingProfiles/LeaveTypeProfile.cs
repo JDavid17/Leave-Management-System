371eba92810a14b0ed2329d7985d76305fb16382
Joel Cruz <joel.cruz@bbconsult.co.uk>
2023-12-01 12:20:38 +0100

    Added Dtos and created AutoMapper Profile mapping

---
+++ LeaveManagement.Application/MappingProfiles/LeaveTypeProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+using LeaveManagement.Domain;
+
+namespace LeaveManagement.Application.MappingProfiles;
+
+public class LeaveTypeProfile : Profile
+{
+    public LeaveTypeProfile()
+    {
+        CreateMap<LeaveTypeDto, LeaveType>().ReverseMap();
+        CreateMap<LeaveType, LeaveTypeDetailDto>();
+    }
+}
